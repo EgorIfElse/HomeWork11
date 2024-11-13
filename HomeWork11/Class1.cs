1d609b0b8f229fa26e244e1985a04083d0be689a
user <user@WIN-PRBG4ODTMQN>
2024-11-13 17:56:04 +0300

    Добавьте файлы проекта.

---
+++ HomeWork11/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal struct Line
+    {
+        double B;
+        double K;
+        public Line(double b, double k) 
+        {  
+            this.B = b;
+            this.K = k;
+          
+        }
+
+        public double Root()
+        {
+            return -B / K;
+        }
+    }
+}
---
+++ HomeWork11/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double B = Convert.ToDouble(Console.ReadLine());
+           double K = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(B, K);
+            Console.WriteLine(line.Root());
+
+        }
+    }
+}
